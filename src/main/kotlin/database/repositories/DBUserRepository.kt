5bcda10aa201eee6ce3262d55fb62fc57636de0e
Stanislav Snagin <s_lstoo@bk.ru>
2026-01-11 21:25:50 +0300

    work in progress

--- src/main/kotlin/database/repositories/DBUserRepository.kt
+++ src/main/kotlin/database/repositories/DBUserRepository.kt
@@ -3,10 +3,8 @@ package database.repositories
 import database.JpaRepository
 import database.model.User
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.inject.Inject
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
-import utils.crypt.PasswordEncoder
 
 @ApplicationScoped
 open class DBUserRepository : JpaRepository<User, Long>() {
